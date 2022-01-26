017732bd1271a503e721acb625d2ffd6cc56ab8c
constracti <constracti@gmail.com>
2022-01-26 23:22:34 +0200

    implement customizations

--- index.php
+++ index.php
@@ -37,3 +37,42 @@ function agonistes_version(): string {
 add_action( 'init', function(): void {
 	load_plugin_textdomain( 'agonistes', FALSE, basename( __DIR__ ) . '/languages' );
 } );
+
+// replace default gallery
+add_action( 'init', function(): void {
+	if ( !shortcode_exists( 'fusion_gallery' ) )
+		return;
+	remove_shortcode( 'gallery' );
+	add_shortcode( 'gallery', function( array $atts ): string {
+		$shortcode = '[fusion_gallery';
+		if ( array_key_exists( 'ids', $atts ) )
+			$shortcode .= sprintf( ' image_ids="%s"', $atts['ids'] );
+		if ( array_key_exists( 'columns', $atts ) )
+			$shortcode .= sprintf( ' columns="%d"', $atts['columns'] );
+		$shortcode .= ' lightbox="yes"][/fusion_gallery]';
+		return do_shortcode( $shortcode );
+	} );
+} );
+
+// accept tag__in variable in main query
+add_filter( 'query_vars', function( array $qvars ): array {
+	$qvars[] = 'tag__in';
+	return $qvars;
+} );
+add_action( 'pre_get_posts', function( WP_Query $query ): void {
+	if ( !$query->is_main_query() )
+		return;
+	if ( !array_key_exists( 'tag__in', $query->query ) )
+		return;
+	$query->query['tag__in'] = explode( '-', $query->query['tag__in'] );
+	$query->parse_query( $query->query );
+} );
+
+// hide closed comment section
+add_action( 'wp_head', function(): void {
+	if ( !is_singular() )
+		return;
+	if ( comments_open() )
+		return;
+	echo '<style type="text/css">#comments{display: none;}</style>' . "\n";
+} );
